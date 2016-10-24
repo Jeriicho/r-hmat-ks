2689cf9d2b49b6cc91290a4e76be2627c0810f86
Marko <markokullam@gmail.com>
2016-10-24 17:07:21 +0300

    Some changes

--- TestBussid.java
+++ TestBussid.java
@@ -2,6 +2,7 @@ package Rühmatöö;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
@@ -15,7 +16,7 @@ import static javafx.scene.input.DataFormat.URL;
 public class TestBussid{
 
     public static void main(String[] args) throws Exception {
-        LuxExpress buss = new LuxExpress("Tallinn", "Tartu", "15", "10");
+        LuxExpress buss = new LuxExpress("Tartu", "Tallinn", "19", "10");
         URL url = new URL(buss.genereeriMaandumisLeht());
         URLConnection connect = url.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader((connect.getInputStream())));
@@ -26,9 +27,18 @@ public class TestBussid{
         in.close();
 
         Document doc = Jsoup.parse(html);
-        Elements elemendid = doc.select(".inactiveHeader");
-        System.out.println(elemendid);
+        Elements elemendid = doc.select(".regular-fullPrice");
+        for (Element element : elemendid) {
+            Elements div = element.children().tagName("div");
+            if (!div.hasClass("sold-out-text")) {
+                System.out.println("Väljumisaeg: ");
+                System.out.println(element.parent().parent().parent().parent().parent().parent().child(0).child(0).child(0).child(0).html()); // 6 parentNode'i, millele järgneb 4 child(0) node'i
+                System.out.println("Piletihind: ");
+                System.out.println(element.child(0).html());
+                System.out.println(" --- ");
+            }
 
-
+        }
+        
     }
 }
